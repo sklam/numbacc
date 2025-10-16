70c4307d0cf063427275de1c1299b4e4b896289b
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2025-10-15 19:14:55 -0500

    more testing

--- nbcc/frontend/tests/test_ast_to_scfg.py
+++ nbcc/frontend/tests/test_ast_to_scfg.py
@@ -29,3 +29,42 @@ def main() -> None:
     print(c)
 """
     _compile(source)
+
+def test_basic_if_nested_1():
+    source = """
+def main() -> None:
+    a: i32
+    b: i32
+    c: i32
+
+    a = 1
+    b = 2
+    if a < b:
+        c = b
+        if a < b * 2:
+            c = c + 1
+        else:
+            return
+    else:
+        c = a
+
+    print(c)
+"""
+    _compile(source)
+
+
+# def test_basic_if():
+#     source = """
+# def main() -> None:
+#     a: i32
+#     b: i32
+#     c: i32
+
+#     a = 1
+#     b = 2
+#     while a < b:
+#         a = a + 1
+
+#     print(a)
+# """
+#     _compile(source)
